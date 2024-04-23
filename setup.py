8721d24dca264852db2f9e34a2cc5849b96af5d5
Sakshi Shrivastava <sakshi.cseng13@gmail.com>
2024-04-23 09:09:25 +0530

    installing local package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+ 
+setup(
+    name='FlightFarePredictionS',
+    version='0.0.1',
+    author='Sakshi Shrivastava',
+    author_email='sakshi.cseng13@gmail.com',
+    packages=find_packages()
+)
